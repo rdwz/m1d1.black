8e8536712081e30e9c6363b2efc3562dc4f640c7
Rychart Redwerkz <redwerkz@users.noreply.github.com>
2023-01-07 13:11:59 +0100

    🔧 Clean astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// import { astroImageTools } from 'astro-imagetools'
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import compressor from 'astro-compressor'
@@ -6,16 +5,14 @@ import robotsTxt from 'astro-robots-txt'
 import sitemap from '@astrojs/sitemap'
 import purgecss from 'astro-purgecss'
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
-		// astroImageTools,
 		compress(),
 		compressor(),
 		purgecss({
 			fontFace: true,
 			keyframes: true,
-			content: ['./public/**/*', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+			content: ['./public/**/*', './src/**/*.{astro,js,jsx,ts,tsx}'],
 			rejected: true,
 			variables: true
 		}),
